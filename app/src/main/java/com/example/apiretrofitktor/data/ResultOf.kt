d7a2a38545f4f2dd1736989200365eb09802a7e5
zzmitzz <ghtk@GHTKs-MacBook-Pro.local>
2024-08-27 19:10:31 +0700

    upate

---
+++ app/src/main/java/com/example/apiretrofitktor/data/ResultOf.kt
@@ -0,0 +1,8 @@
+package com.example.apiretrofitktor.data
+
+
+sealed class ResultOf<out T> {
+    data class Success<out R>(val data: R) : ResultOf<R>()
+    data class Error(val message: String, val exception: Exception) : ResultOf<Nothing>()
+    object Loading: ResultOf<Nothing>()
+}
